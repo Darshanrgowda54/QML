29d8cd2b747fa1ffc71ae4da784fd401aafbb7d7
Darshanrgowda54 <darshan.ramesh565@gmail.com>
2025-08-10 19:31:39 +0530

    Student Registration Application

---
+++ Registration_Application/main.cpp
@@ -0,0 +1,28 @@
+#include "Student.h"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+int main(int argc, char *argv[])
+{
+    Student loginHandler;
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    QQmlContext* context = engine.rootContext();
+    context->setContextProperty("Login",&loginHandler);
+
+    const QUrl url(QStringLiteral("qrc:/Registration_Application/main.qml"));
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
