2568b7f9e8f0f6fc920f1926ef296312426ff2d0
Darshanrgowda54 <darshan.ramesh565@gmail.com>
2025-08-28 10:43:56 +0530

    ContactListViewTask

---
+++ ContactListTask/main.cpp
@@ -0,0 +1,28 @@
+#include "ContactDataModel.h"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    ContactDataModel datamodel;
+    QQmlApplicationEngine engine;
+
+    engine.rootContext()->setContextProperty("Data",&datamodel);
+
+    const QUrl url(QStringLiteral("qrc:/ContactListTask/main.qml"));
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
